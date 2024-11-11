5b9d03b4a243cba9999edcb94db2d13db7cc8cd3
Gitika Pinjani <gitika.raipur123@gmail.com>
2024-11-11 23:53:33 +0530

    first commit

---
+++ src/main/java/com/gitikapinjani/yummyrestaurant/YummyRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.gitikapinjani.yummyrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyRestaurantApplication.class, args);
+    }
+
+}
