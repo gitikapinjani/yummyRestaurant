3345bb7a8e84b68fd901eecf53ed5e49c88602e8
Gitika Pinjani <gitika.raipur123@gmail.com>
2024-11-21 01:12:54 +0530

    Increased token expiration time

--- src/main/java/com/gitikapinjani/yummyrestaurant/helper/JWTHelper.java
+++ src/main/java/com/gitikapinjani/yummyrestaurant/helper/JWTHelper.java
@@ -40,7 +40,7 @@ public class JWTHelper {
     // Create token with claims
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60)) // Token valid for 10 hours
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
     // Validate token
